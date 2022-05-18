634354dd9486bc805fb12637aecd30c1d8c6e9ef
david128 <davidireland128@gmail.com>
2022-05-18 17:25:16 +0100

    refactored to better class hierarchy

--- asteroids.py
+++ asteroids.py
@@ -6,6 +6,7 @@ pygame.init()
 
 gameover = False
 bullets = []
+alienBullets = []
 asteroids =[]
 newAsteroids =[]
 score = 0
@@ -26,7 +27,55 @@ win = pygame.display.set_mode((screenW,screenH))
 
 clock = pygame.time.Clock()
 
-class Player(object):
+
+
+
+
+class gameObject(object):
+    def __init__(self):
+        self.x
+        self.y
+
+    def move(self):
+        self.x += self.xVelocity
+        self.y -= self.yVelocity
+
+    def isOffScreen(self):
+        if self.x <-50 or self.x > screenW+50 or self.y <-50 or self.y >screenH+50:
+            return True
+
+class Bullet(gameObject):
+
+    def __init__(self):
+        self.x,self.y = player.head
+        self.cosine = player.cosine
+        self.sine = player.sine
+        self.size = 3
+        self.xVelocity = 10*self.cosine
+        self.yVelocity = 10*self.sine
+        super().__init__()
+
+    def draw(self, win):
+        pygame.draw.rect(win,(255,255,255),[self.x,self.y,self.size,self.size])
+
+class AlienBullet(gameObject):
+    def __init__(self):
+        self.x = alien.x
+        self.y = alien.y
+        self.size = 3
+
+        self.dx, self.dy = player.x - self.x, player.y - self.y
+        self.dist = math.hypot(self.dx, self.dy)
+        self.dx, self.dy = self.dx / self.dist, self.dy / self.dist
+        self.xVelocity = self.dx * 5
+        self.yVelocity = self.dy * 5
+
+        super().__init__()
+
+    def draw(self, win):
+        pygame.draw.rect(win,(255,255,255),[self.x,self.y,self.size,self.size])
+
+class Player(gameObject):
     def __init__(self):
         self.img = shipImage
         self.w = self.img.get_width()
@@ -40,6 +89,9 @@ class Player(object):
         self.cosine = math.cos(math.radians(self.angle + 90))
         self.sine = math.sin(math.radians(self.angle + 90))
         self.head = (self.x + self.cosine * self.w/2, self.y -self.sine * self.h/2)
+        self.velocityX =0
+        self.velocityY =0
+        super().__init__()
 
     def draw(self,win):
         win.blit(self.rotatedSurface,self.rotatedRectangle)
@@ -54,16 +106,18 @@ class Player(object):
 
     def turnLeft(self):
         self.angle += 5
-        self.updateAngle()
 
     def turnRight(self):
         self.angle -= 5
+
+    def move(self):
+        self.x += self.velocityX
+        self.y += self.velocityY
         self.updateAngle()
 
     def moveForward(self):
-        self.x += self.cosine * 6
-        self.y -= self.sine * 6
-        self.updateAngle()
+        self.velocityX += self.cosine *0.1
+        self.velocityY -= self.sine *0.1
 
     def checkPos(self):
         if self.x <-50:
@@ -81,51 +135,27 @@ class Player(object):
         self.angle= 0
         self.updateAngle()
 
-class Bullet(object):
-
-    def __init__(self):
-        self.x,self.y = player.head
-        self.cosine = player.cosine
-        self.sine = player.sine
-        self.size = 3
-        self.xVelocity = 10*self.cosine
-        self.yVelocity = 10*self.sine
-
-    def move(self):
-        self.x += self.xVelocity
-        self.y -= self.yVelocity
-
-    def draw(self, win):
-        pygame.draw.rect(win,(255,255,255),[self.x,self.y,self.size,self.size])
-
-    def isOffScreen(self):
-        if self.x <-50 or self.x > screenW+50 or self.y <-50 or self.y >screenH+50:
-            return True
-
-class Asteroid(object):
-    def __init__(self, x,y):
+class NonPlayerObject(gameObject):
+    def __init__(self, x, y):
         self.x = x
         self.y = y
-        if(x==0 and y==0):
+        if (x == 0 and y == 0):
             self.randomSpawn()
-        if (self.x < screenW/2):
-            #on left side
+        if (self.x < screenW / 2):
+            # on left side
             self.xdir = 1
         else:
-            self.xdir=-1
+            self.xdir = -1
         if (self.y < screenH / 2):
             # on top side
             self.ydir = 1
         else:
             self.ydir = -1
 
-
-
     def randomSpawn(self):
-        #find random pos to spawn asteroid at
-        self.spawnPos =random.choice([(random.choice([-50,screenW+50]),random.randrange(0,screenH)),
-                                      (random.randrange(0,screenW),random.choice([-50,screenH+50]))])
-
+        # find random pos to spawn asteroid at
+        self.spawnPos = random.choice([(random.choice([-50, screenW + 50]), random.randrange(0, screenH)),
+                                       (random.randrange(0, screenW), random.choice([-50, screenH + 50]))])
         self.x, self.y = self.spawnPos
 
     def move(self):
@@ -135,8 +165,20 @@ class Asteroid(object):
     def draw(self, win):
         win.blit(self.img, (self.x, self.y))
 
+
+class Asteroid(NonPlayerObject):
+    def __init__(self, x,y):
+        super().__init__(x,y)
+
     def hit(self):
         pass
+
+class Alien(NonPlayerObject):
+    def __init__(self,x,y):
+        self.img = shipImage
+        self.xVelocity = 0
+        self.yVelocity = 0
+        super().__init__(x,y)
 
 class smallAsteroid(Asteroid):
     def __init__(self,x,y):
@@ -147,7 +189,6 @@ class smallAsteroid(Asteroid):
         super().__init__(x,y)
 
     def hit(self):
-
         super().hit()
 
 class mediumAsteroid(Asteroid):
@@ -188,17 +229,26 @@ def resetGame():
 
     asteroids.clear()
     bullets.clear()
+    alienBullets.clear()
 
 def redrawWindow():
     win.blit(bg,(0,0))
     font = pygame.font.SysFont('arial',20)
     livesText = font.render('Lives: '+ str(lives),1,(255,255,255))
     player.draw(win)
+    alien.draw(win)
     for b in bullets:
         if b.isOffScreen():
             bullets.pop(bullets.index(b))
         else:
             b.draw(win)
+    for ab in alienBullets:
+        if ab.isOffScreen():
+            alienBullets.pop(alienBullets.index(ab))
+        else:
+            ab.draw(win)
+            print("dsakdja")
+
     for a in asteroids:
         a.draw(win)
     win.blit(livesText,(25,25))
@@ -213,6 +263,7 @@ def collisionCheck(ax,ay,asize,bx,by,bsize) :
 
 
 player = Player()
+alien = Alien(0,0)
 run = True
 print("hello")
 while run:
@@ -221,6 +272,9 @@ while run:
     if not gameover:
         if count % 50 == 0:
             asteroids.append(random.choice([smallAsteroid(0,0),mediumAsteroid(0,0),largeAsteroid(0,0)]))
+            alienBullets.append(AlienBullet())
+
+
         for b in bullets:
             b.move()
         for a in asteroids:
@@ -242,8 +296,10 @@ while run:
         for n in newAsteroids:
             asteroids.append(n)
 
-
+        player.move()
+        alien.move()
         player.checkPos()
+
         inputs = pygame.key.get_pressed()
         if inputs[pygame.K_LEFT]:
             player.turnLeft()
