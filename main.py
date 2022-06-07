b8a484800a5799b8f4c19018deb5f8b6d29eda63
david128 <davidireland128@gmail.com>
2022-06-07 14:00:00 +0100

    removed leftover code

--- main.py
+++ main.py
@@ -7,15 +7,6 @@ from stable_baselines3 import PPO
 import os
 import time
 
-
-models_dir = f"models/PPO-{int(time.time())}"
-log_dir = f"logs/PPO-{int(time.time())}"
-
-if  not os.path.exists(models_dir):
-    os.makedirs(models_dir)
-if not os.path.exists(log_dir):
-    os.makedirs(log_dir)
-
 env = astEnviroment.asteroidEnv()
 env.reset()
 obs = env.reset()
